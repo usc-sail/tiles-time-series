3cefb89818775aab9f684a250bb2212dd83d92c0
tiantiaf0627 <tiantiaf@usc.edu>
2019-02-14 16:01:07 -0800

    delete old files

--- individual_timeline/individual_timeline.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-# Extract individual timeline
-mainfoldervar=$1
-outputfoldervar=$2
-
-# sh individual_timeline.sh ../../data ../output
-python3.6 individual_timeline.py -i $mainfoldervar -o $outputfoldervar
